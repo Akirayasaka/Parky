a3b45d62d8468dcb51c19b5a5652ccdc88a8f03a
Akira <kuramal12@msn.com>
2022-02-08 10:39:00 +0800

    Add Repository

---
+++ ParkyAPI/Repository/IRepository/IRepository.cs
@@ -0,0 +1,47 @@
+﻿using System.Linq.Expressions;
+
+namespace ParkyAPI.Repository.IRepository
+{
+    public interface IRepository<T> where T : class
+    {
+        /// <summary>
+        /// 根據類別取資料
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        T Get(int id);
+
+        /// <summary>
+        /// 根據類別取得全部資料
+        /// </summary>
+        /// <returns></returns>
+        ICollection<T> GetAll(Expression<Func<T, bool>>? filter,
+            Func<IQueryable<T>, IOrderedQueryable<T>>? orderBy);
+
+        /// <summary>
+        /// 根據條件篩選
+        /// </summary>
+        /// <param name="filter"></param>
+        /// <param name="includeProperties"></param>
+        /// <returns></returns>
+        T GetFirstOrDefault(Expression<Func<T, bool>>? filter, string? includeProperties);
+
+        /// <summary>
+        /// 新增資料
+        /// </summary>
+        /// <param name="entity"></param>
+        void Add(T entity);
+
+        /// <summary>
+        /// 刪除資料
+        /// </summary>
+        /// <param name="id"></param>
+        void Remove(int id);
+
+        /// <summary>
+        /// 刪除資料
+        /// </summary>
+        /// <param name="entity"></param>
+        void Remove(T entity);
+    }
+}
---
+++ ParkyAPI/Repository/Repository.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ParkyAPI.Repository.IRepository;
+using System.Linq.Expressions;
+
+namespace ParkyAPI.Repository
+{
+    public class Repository<T> : IRepository<T> where T : class
+    {
+        protected readonly DbContext _db;
+        internal DbSet<T> DbSet;
+
+        public Repository(DbContext db)
+        {
+            _db = db;
+            DbSet = db.Set<T>();
+        }
+
+        public void Add(T entity)
+        {
+            DbSet.Add(entity);
+        }
+
+        public T Get(int id)
+        {
+            return DbSet.Find(id);
+        }
+
+        public ICollection<T> GetAll(Expression<Func<T, bool>>? filter,
+            Func<IQueryable<T>, IOrderedQueryable<T>>? orderBy)
+        {
+            IQueryable<T> query = DbSet;
+            if (filter != null)
+            {
+                query = query.Where(filter);
+            }
+            return orderBy != null ? orderBy(query).ToList() : query.ToList();
+        }
+
+        public T GetFirstOrDefault(Expression<Func<T, bool>>? filter, string? includeProperties)
+        {
+            IQueryable<T> query = DbSet;
+            if (filter != null)
+            {
+                query = query.Where(filter);
+            }
+            return query.FirstOrDefault();
+        }
+
+        public void Remove(int id)
+        {
+            T target = DbSet.Find(id);
+            DbSet.Remove(target);
+        }
+
+        public void Remove(T entity)
+        {
+            DbSet.Remove(entity);
+        }
+    }
+}
