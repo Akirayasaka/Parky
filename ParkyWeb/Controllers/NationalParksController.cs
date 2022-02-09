bbcf44bc9e3feba094915840ac223af6a6e3ee93
Akira <kuramal12@msn.com>
2022-02-09 12:47:22 +0800

    Add liberys, ViewComponents;

---
+++ ParkyWeb/Controllers/NationalParksController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ParkyWeb.Repository.IRepository;
+
+namespace ParkyWeb.Controllers
+{
+    public class NationalParksController : Controller
+    {
+        private readonly IUnitOfWork _unitOfWork;
+
+        public NationalParksController(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public async Task<IActionResult> GetAllNationalPark()
+        {
+            return Json(new { data = await _unitOfWork.NationalPark.GetAllAsync(SD.NationalParkAPIPath)});
+        }
+    }
+}
